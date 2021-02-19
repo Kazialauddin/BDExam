d8e3f5c34f942450f6a363c5ea677ae872cde9a0
kazialauddin <kazi.alauddin421@gmail.com>
2021-02-19 22:52:32 +0600

    Set up

---
+++ OnlineExamBD/OnlineExamBD/Models/AppDbCOntext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace OnlineExamBD.Models
+{
+    public class AppDbCOntext:DbContext
+    {
+        public AppDbCOntext() : base("DefaultConnection") 
+        {
+            //"https://www.youtube.com/watch?v=p9GdkmFzoPw"
+        }
+    }
+}
